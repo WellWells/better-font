0965f860229f2387915cf570f6291cca8119a57d
Wells <76758648+WellWells@users.noreply.github.com>
2024-05-24 14:18:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [css] BetterFont
 // @homepageURL  https://github.com/WellWells/better-font
-// @version      20231228
+// @version      20240524
 // @description  Improved Font Rendering and Style Customization for a Better Reading Experience
 // @author       WellsTsai
 // @include      *://*
@@ -11,29 +11,34 @@
 (function () {
     "use strict";
 
-    // Change the flag is want to debug.
-    const IS_DEBUG = true
+    // 設定是否啟用除錯模式
+    const IS_DEBUG = true;
 
+    // 黑名單網站列表
     const BLACK_LIST = [
         "leetcode.com", "regex101.com", "w3schools.com",
         "bobondemon.github.io", "iamchucky.github.io", "pictogrammers.github.io",
         "fonts", "justfont", "materialdesignicons.com",
         "docs.google.com", "remotedesktop.google.com", "developers.google.com", "cloud.google.com", "play.google.com",
         "icloud.com", "microsoft.com", "live.com", "office.com",
-        "term.ptt.cc", "wellstsai.com",
+        "term.ptt.cc"
     ];
 
+    // 語言黑名單
     const LANG_LIST = ["fa-ir", "ru"];
+
+    // 獲取當前網站和語言
     const URL = window.location.hostname;
     const LANG = document.documentElement.lang.toLowerCase();
-    const URL_MATCH = RegExp(BLACK_LIST.join("|")).exec(URL);
-    const LANG_MATCH = RegExp(LANG_LIST.join("|")).exec(LANG);
 
-    if (URL_MATCH || LANG_MATCH) {
-        if (IS_DEBUG) console.log(`[Font/Debug] The font remains unchanged due to:${URL_MATCH ? URL_MATCH : LANG_MATCH}`);
-        return false;
+    // 匹配黑名單
+    const isBlacklisted = (list, value) => list.some(item => value.includes(item));
+    if (isBlacklisted(BLACK_LIST, URL) || isBlacklisted(LANG_LIST, LANG)) {
+        if (IS_DEBUG) console.log(`[Font/Debug] 字體未更改，因為在黑名單中: ${URL} 或語言: ${LANG}`);
+        return;
     }
 
+    // 過濾器配置
     const FILTER = {
         class: [
             "K3JSBVB-r-e", "O4", "rating", "vjs", "jump-top-box", "social_btn",
@@ -42,21 +47,24 @@
             "btn", "indicator", "Logo", "logo", "lg-icon", "_3Kzp1", "ni", "sf",
             "se", "anchorjs", "fui", "nav-toggle", "fc_meta", "show", "i-", "mjx", "docon", "o365cs"
         ],
-        eventaction: ["search"],
-    }
+        eventaction: ["search"]
+    };
 
     const IGNORE_TAGS = ["i", "em", "button", "font", "span", "svg", "blockquote"];
 
-    function getIgnoredSelectors(filter) {
-        return Object.entries(filter).map(([key, values]) => {
-            return values.map(value => `:not([${key}*="${value}"])`).join("");
-        }).join("");
-    }
+    // 生成忽略選擇器
+    const getIgnoredSelectors = (filter) => {
+        return Object.entries(filter).map(([key, values]) =>
+            values.map(value => `:not([${key}*="${value}"])`).join("")
+        ).join("");
+    };
 
+    // 設置樣式
     const ignoredSelectors = getIgnoredSelectors(FILTER);
     const ignoredTags = IGNORE_TAGS.map(tag => `:not(${tag})`).join("");
     const css = document.createElement("style");
-    css.innerText = `*${ignoredSelectors}${ignoredTags}{font-family: "LiHei Pro" !important; font-weight: normal !important;} textarea {font-family: inherit!important;}`;
+    css.innerText = `*${ignoredSelectors}${ignoredTags} { font-family: "LiHei Pro" !important; font-weight: normal !important; } textarea { font-family: inherit !important; }`;
     document.head.appendChild(css);
-    console.log(css.innerText);
+
+    if (IS_DEBUG) console.log(css.innerText);
 })();
