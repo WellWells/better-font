8dc3f10b43f2d0a8c3e620f94d1edff8278632a7
Wells <76758648+WellWells@users.noreply.github.com>
2024-06-04 14:49:46 +0800

    Add monospace filter

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [css] BetterFont
 // @homepageURL  https://github.com/WellWells/better-font
-// @version      20240524
+// @version      20240604
 // @description  Improved Font Rendering and Style Customization for a Better Reading Experience
 // @author       WellsTsai
 // @include      *://*
@@ -20,7 +20,12 @@
         "bobondemon.github.io", "iamchucky.github.io", "pictogrammers.github.io",
         "fonts", "justfont", "materialdesignicons.com",
         "docs.google.com", "remotedesktop.google.com", "developers.google.com", "cloud.google.com", "play.google.com",
-        "icloud.com", "microsoft.com", "live.com", "office.com", "term.ptt.cc"
+        "icloud.com", "microsoft.com", "live.com", "office.com",
+        "term.ptt.cc", "wellstsai.com"
+    ];
+
+    const MONOSPACE_PAGE = [
+        "wikipedia.org"
     ];
 
     // 語言黑名單
@@ -32,6 +37,9 @@
 
     // 匹配黑名單
     const isBlacklisted = (list, value) => list.some(item => value.includes(item));
+    // 匹配 Monospace 名單
+    const isPageUseMonospace = (list, value) => list.some(item => value.includes(item));
+
     if (isBlacklisted(BLACK_LIST, URL) || isBlacklisted(LANG_LIST, LANG)) {
         if (IS_DEBUG) console.log(`[Font/Debug] 字體未更改，因為在黑名單中: ${URL} 或語言: ${LANG}`);
         return;
@@ -44,7 +52,7 @@
             "pe-7s", "la", "fa", "f7-icons", "Icon", "ico", "material-icons",
             "DPvwYc", "Xm9Bod", "ynvm8", "goog-inline-block", "share", "article-vote-controls",
             "btn", "indicator", "Logo", "logo", "lg-icon", "_3Kzp1", "ni", "sf",
-            "se", "anchorjs", "fui", "nav-toggle", "fc_meta", "show", "i-", "mjx", "docon", "o365cs", "typography-feature-check"
+            "se", "anchorjs", "fui", "nav-toggle", "fc_meta", "show", "i-", "mjx", "docon", "o365cs"
         ],
         eventaction: ["search"]
     };
@@ -62,7 +70,10 @@
     const ignoredSelectors = getIgnoredSelectors(FILTER);
     const ignoredTags = IGNORE_TAGS.map(tag => `:not(${tag})`).join("");
     const css = document.createElement("style");
-    css.innerText = `*${ignoredSelectors}${ignoredTags} { font-family: "LiHei Pro" !important; font-weight: normal !important; } textarea { font-family: inherit !important; }`;
+
+    const fontFamily = isPageUseMonospace(MONOSPACE_PAGE, URL) ? `"Noto Sans Mono", "Noto Sans TC"` : `"lihei pro"`
+    css.innerText = `*${ignoredSelectors}${ignoredTags} { font-family: ${fontFamily} !important; font-weight: normal !important; } textarea { font-family: inherit !important; }`;
+
     document.head.appendChild(css);
 
     if (IS_DEBUG) console.log(css.innerText);
